193cb3c66336e3ca6029a15d2cbc4403f7f9c87e
psncode <54549762+psncode@users.noreply.github.com>
2023-10-11 11:33:37 +0300

    Add files via upload

---
+++ btckeygen.py
@@ -0,0 +1,196 @@
+import sys
+import tempfile
+import base64
+import string
+import tkinter as tk
+from tkinter import ttk
+from tkinter.constants import DISABLED, E, END, NORMAL, NW, VERTICAL
+import os
+import hashlib
+import binascii
+import ecdsa
+from typing import  Union
+
+
+BITCOIN_ALPHABET = \
+    b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+RIPPLE_ALPHABET = b'rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz'
+XRP_ALPHABET = RIPPLE_ALPHABET
+
+alphabet = BITCOIN_ALPHABET
+
+
+def scrub_input(v: Union[str, bytes]) -> bytes:
+    if isinstance(v, str):
+        v = v.encode('ascii')
+
+    return v
+
+
+def b58encode_int(
+    i: int, default_one: bool = True, alphabet: bytes = BITCOIN_ALPHABET
+) -> bytes:
+
+    if not i and default_one:
+        return alphabet[0:1]
+    string = b""
+    base = len(alphabet)
+    while i:
+        i, idx = divmod(i, base)
+        string = alphabet[idx:idx+1] + string
+    return string
+
+
+def b58encode(
+    v: Union[str, bytes], alphabet: bytes = BITCOIN_ALPHABET
+) -> bytes:
+
+    v = scrub_input(v)
+
+    origlen = len(v)
+    v = v.lstrip(b'\0')
+    newlen = len(v)
+
+    acc = int.from_bytes(v, byteorder='big')  # first byte is most significant
+
+    result = b58encode_int(acc, default_one=False, alphabet=alphabet)
+    return alphabet[0:1] * (origlen - newlen) + result
+
+
+def b58encode_check(
+    v: Union[str, bytes], alphabet: bytes = BITCOIN_ALPHABET
+) -> bytes:
+
+    v = scrub_input(v)
+
+    digest = sha256(sha256(v).digest()).digest()
+    return b58encode(v + digest[:4], alphabet=alphabet)
+
+def ripemd160(x):
+    d = hashlib.new('ripemd160')
+    d.update(x)
+    return d
+
+def newPair():
+    priv_key = genPriv()
+    wif = getWif(priv_key)
+    addr = getAddr(getPub(priv_key))
+  
+    a = "Bitcoin Address:\n" + addr.decode() +"\nPrivate Key (WIF):\n" + wif.decode()
+   
+    return a
+	
+   
+
+def genPriv():
+    priv_key = os.urandom(32)
+    return priv_key
+
+def getWif(priv_key):
+    fullkey = '80' + binascii.hexlify(priv_key).decode()
+    
+    sha256a = hashlib.sha256(binascii.unhexlify(fullkey)).hexdigest()
+    sha256b = hashlib.sha256(binascii.unhexlify(sha256a)).hexdigest()
+    wif = b58encode(binascii.unhexlify(fullkey+sha256b[:8]))
+    return wif
+
+def getPub(priv_key):
+    sk = ecdsa.SigningKey.from_string(priv_key, curve=ecdsa.SECP256k1)
+    vk = sk.get_verifying_key()
+    publ_key = '04' + binascii.hexlify(vk.to_string()).decode()
+    return publ_key
+
+def getAddr(publ_key):
+    hash160 = ripemd160(hashlib.sha256(binascii.unhexlify(publ_key)).digest()).digest()
+    publ_addr_a = b"\x00" + hash160
+    checksum = hashlib.sha256(hashlib.sha256(publ_addr_a).digest()).digest()[:4]
+    publ_addr_b = b58encode(publ_addr_a + checksum)
+    return publ_addr_b
+
+
+
+
+class GUI(tk.Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
+        self.widget_vars()
+        self.create_widgets()
+        self.style()
+
+    def generate_password(self):
+        passw = Password(self.length.get(), self.lower.get(), self.upper.get(),
+                         self.digits.get(), self.punct.get())
+        # You can only insert to Text if the state is NORMAL
+        self.password_text.config(state=NORMAL)
+        self.password_text.delete("1.0", END)   # Clears out password_text
+        self.password_text.insert(END, passw.password)
+        self.password_text.config(state=DISABLED)
+
+    def widget_vars(self):
+        self.length = tk.IntVar(self, value=16)
+        self.lower = tk.BooleanVar(self, value=True)
+        self.upper = tk.BooleanVar(self, value=True)
+        self.digits = tk.BooleanVar(self, value=True)
+        self.punct = tk.BooleanVar(self, value=True)
+
+    def create_widgets(self):
+        # Define widgets
+   
+        self.generate_btn = ttk.Button(self, text="Generate address",
+                                       command=self.generate_password)
+        self.password_text = tk.Text(self, height=5, width=55, state=DISABLED)
+
+        # Place widgets on the screen
+
+        self.generate_btn.grid(columnspan=5, row=4, padx=4, pady=2)
+        self.password_text.grid(columnspan=5, row=6, padx=4, pady=2)
+
+        self.grid(padx=10, pady=10)
+
+    def style(self):
+        self.style = ttk.Style(self)
+        self.style.theme_use("clam")
+
+
+class Password:
+
+    def __init__(self, length: int,
+                 allow_lowercase: bool,
+                 allow_uppercase: bool,
+                 allow_digits: bool,
+                 allow_punctuation: bool) -> None:
+        self.length = length
+        self.allow_lowercase = allow_lowercase
+        self.allow_uppercase = allow_uppercase
+        self.allow_digits = allow_digits
+        self.allow_punctuation = allow_punctuation
+        self.allowed_chars = self.gen_allowed_chars()
+        self.password = self.gen_password()
+
+    def gen_allowed_chars(self) -> str:
+        # I use a string, because random.choice doesn't work with sets:
+        chars = ''
+        if self.allow_lowercase:
+            chars += string.ascii_lowercase
+        if self.allow_uppercase:
+            chars += string.ascii_uppercase
+        if self.allow_digits:
+            chars += string.digits
+        if self.allow_punctuation:
+            chars += string.punctuation
+        return chars
+
+    def gen_password(self) -> str:
+        password = ''
+        for _ in range(self.length):
+            password = newPair()
+        return password
+
+
+
+root = tk.Tk()
+root.title("Bitcoin address generator")
+app = GUI(root)
+app.mainloop()
